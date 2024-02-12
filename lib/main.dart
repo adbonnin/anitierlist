8517982745792c42b4858b6895dc9560e66c6774
Adrien Bonnin <adbonnin@users.noreply.github.com>
2024-02-12 18:52:53 +0100

    :sparkles: Ajout de Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
+import 'package:anitierlist/firebase_options.dart';
 import 'package:anitierlist/src/features/anime/presentation/tierlist/anime_tierlist_screen.dart';
 import 'package:anitierlist/src/l10n/app_localizations.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(
     const ProviderScope(
       child: MyApp(),
